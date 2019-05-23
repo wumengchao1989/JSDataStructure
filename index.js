92d52c8da4c4b4494506c305e8014add42835134
吴梦超 <>
2019-05-23 14:18:13 +0800

    upd

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
+/*
 import BinaryTree from './BinaryTree';
 let bTree=new BinaryTree(0);
 for(let i=0;i<1000;i++){
     bTree.insertBST(Math.random()*3433)
 }
 bTree.findMax(bTree.root);
-document.write(bTree.max);
+document.write(bTree.max);*/
+let impl={
+    helloWorld:function(){
+        console.log("hello world");
+    }
+};
+
+function test(){
+    this.helloWorld();
+}
+var NODE_ENV = process.env.NODE_ENV;
+var invariant = function(condition, format, a, b, c, d, e, f) {
+    if (NODE_ENV !== 'production') {
+        if (format === undefined) {
+            throw new Error('invariant requires an error message argument');
+        }
+    }
+
+    if (!condition) {
+        var error;
+        if (format === undefined) {
+            error = new Error(
+                'Minified exception occurred; use the non-minified dev environment ' +
+                'for the full error message and additional helpful warnings.'
+            );
+        } else {
+            var args = [a, b, c, d, e, f];
+            var argIndex = 0;
+            error = new Error(
+                format.replace(/%s/g, function() { return args[argIndex++]; })
+            );
+            error.name = 'Invariant Violation';
+        }
+
+        error.framesToPop = 1; // we don't care about invariant's own frame
+        throw error;
+    }
+};
+invariant(false,"this is %s an error %s","asdd","test b");
+Object.assign(test.prototype,impl)
+
+let a=new test();
--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+/*
 let p1=new Promise((resolve)=>{
     let a=parseInt(Math.random()*5000,10);
     console.log("a origin",a)
@@ -31,4 +32,61 @@ let p=Promise.race([p1,p2,p3]);
 p.then((f)=>{
     f();
 });
+*/
 
+
+function quickSort(arr){
+    if (arr.length <= 1) { return arr; }
+    let left=[];
+    let right=[];
+    let pivot=arr[arr.length-1];
+    for(let i=0;i<arr.length-2;i++){
+        if(arr[i]<pivot){
+            left.push(arr[i])
+        }else{
+            right.push(arr[i])
+        }
+    }
+    console.log(left)
+    console.log(right)
+    return quickSort(left).concat([pivot], quickSort(right));
+}
+
+
+var quickSortE = function(arr) {
+
+    if (arr.length <= 1) { return arr; }
+
+    var pivotIndex = Math.floor(arr.length / 2);
+
+    var pivot = arr.splice(pivotIndex, 1)[0];
+
+    var left = [];
+
+    var right = [];
+
+    for (var i = 0; i < arr.length; i++){
+
+        if (arr[i] < pivot) {
+
+            left.push(arr[i]);
+
+        } else {
+
+            right.push(arr[i]);
+
+        }
+
+    }
+    console.log(left);
+    console.log(right)
+
+    return quickSort(left).concat([pivot], quickSort(right));
+
+};
+
+
+
+
+
+console.log('sorted',quickSortE([16,5,11,23,44,55]))
